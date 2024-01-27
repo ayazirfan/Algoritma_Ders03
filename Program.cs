6481d4491f1c88206ab7af30bc4289be6cf1b590
ayazirfan <byirfanayaz@gmail.com>
2024-01-27 16:11:12 +0300

    BTK Akademi

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using AlgoritmaDersler;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Ders03.Anakod();
+        var mC = new SampleClass(11,22);
+        Console.WriteLine($"x={mC.x}, y={mC.y}");
+        Console.WriteLine($"C1={SampleClass.c1}, C2={SampleClass.c2}");
+    }
+}
+class SampleClass
+{
+    public int x;
+    public int y;
+
+    public const int c1 = 5;
+    public const int c2 = c1 + 5;
+    public SampleClass(int p1, int p2)
+    {
+        x= p1;
+        y= p2;
+    }
+
+
+}
